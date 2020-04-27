0541ba6696062308f5563a7bf5899668562d6434
ejazsayyad <ejazsayyad4@gmail.com>
2020-04-27 20:46:54 +0530

    Updates

---
+++ precache-manifest.4a0dab9f9868246c035476e307f23bdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "044a0657592590bd62cf8bb10ab1ae89",
+    "url": "/ImageSearch-v2/index.html"
+  },
+  {
+    "revision": "5f91c6ff5228721f68e8",
+    "url": "/ImageSearch-v2/static/css/main.672a67c9.chunk.css"
+  },
+  {
+    "revision": "628425be72ad922c2270",
+    "url": "/ImageSearch-v2/static/js/2.6b5d0544.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ImageSearch-v2/static/js/2.6b5d0544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f91c6ff5228721f68e8",
+    "url": "/ImageSearch-v2/static/js/main.1b3d9cf9.chunk.js"
+  },
+  {
+    "revision": "923592861986d2ff3a32",
+    "url": "/ImageSearch-v2/static/js/runtime-main.d649c4f1.js"
+  },
+  {
+    "revision": "045ee8f82cd009ce9a322cb8ae63e305",
+    "url": "/ImageSearch-v2/static/media/imagle.045ee8f8.png"
+  }
+]);
